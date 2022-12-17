ea7dcd4236bb2d8f8a15af4859cd3d746336dae3
SuhailSherief <suhailscreed@gmail.com>
2022-12-17 22:59:38 +0530

    changed description

--- pages/index.tsx
+++ pages/index.tsx
@@ -73,11 +73,10 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
+              I'm a full-stack web developer, skilled in building web apps with
+              <span className="text-teal-500"> react, nextJS, vue </span>
+              front-end and<span className="text-teal-500"> express, django, flask </span>
+              backend and have collaborated with talanted people to create digital products
               for both business and consumer use.
             </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
