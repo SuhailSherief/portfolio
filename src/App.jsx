54c773cbaba6e414f17a598b54bf1770b8da6864
SuhailSherief <suhailscreed@gmail.com>
2023-02-07 06:41:31 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './App.css'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+import Projects from './components/projects/Projects'
+import Testimonials from './components/testimonials/Testimonials'
+
+const App = () => {
+  return (
+    <>
+        <Header />
+        <Navbar />
+        <About />
+        <Experience />
+        <Projects />
+        <Testimonials />
+        <Contact />
+        <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Experience.module.css'
+
+const Experience = () => {
+  return (
+    <div>Experience</div>
+  )
+}
+
+export default Experience
