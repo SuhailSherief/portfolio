951e8c7a8d2e6eb6adb29ca39f213d9292d48fde
SuhailSherief <suhailscreed@gmail.com>
2023-02-15 08:05:07 +0530

    deleted unwanted files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import './App.css'
-import About from './components/about/About'
-import Contact from './components/contact/Contact'
-import Experience from './components/experience/Experience'
-import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
-import Navbar from './components/navbar/Navbar'
-import Projects from './components/projects/Projects'
-import Testimonials from './components/testimonials/Testimonials'
+import Home from './components/home/Home'
 
 const App = () => {
   return (
     <>
         <Header />
+        <main className='main'>
+        <Home />
+        </main>
     </>
   )
 }
