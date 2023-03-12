d7874fcb53ce5bca61b93cd16578e2d108151a5f
SuhailSherief <suhailscreed@gmail.com>
2023-03-12 18:20:02 +0530

    Added skills page

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -5,7 +5,7 @@ import Backend from './Backend'
 
 function Skills() {
   return (
-    <section className="skills section">
+    <section className="skills section" id="skills">
         <h2 className="section__title">Skills</h2>
         <span className="section__subtitle">Technical level</span>
         <div className="skills__container container grid">
