161d7a8c6eacf6a21ce6c878505d72928109e70c
SuhailSherief <suhailscreed@gmail.com>
2023-03-12 18:21:20 +0530

    Added project data

---
+++ src/components/projects/Data.jsx
@@ -0,0 +1,46 @@
+import work1 from '../../assets/work1.jpg'
+import work2 from '../../assets/work2.png'
+import work3 from '../../assets/work3.png'
+import work4 from '../../assets/work4.png'
+
+export const projectsData = [
+    {
+      id: 1,
+      image: work1,
+      title: "User Repository",
+      category: "web",
+    },
+    {
+      id: 2,
+      image: work2,
+      title: "Vendor Service Portal",
+      category: "web",
+    },
+    {
+      id: 3,
+      image: work3,
+      title: "Opencore x260",
+      category: "others",
+    },
+    {
+      id: 4,
+      image: work4,
+      title: "Twitter Clone",
+      category: "web",
+    }
+  ];
+  
+export const projectsNav = [
+    {
+        name: 'all'
+    },
+    {
+        name: 'web'
+    },
+    {
+        name: 'design'
+    },
+    {
+        name: 'others'
+    }
+]
