88446b56ecb721d362f9962adc1ceb9832113bb1
SuhailSherief <suhailscreed@gmail.com>
2023-03-12 18:21:36 +0530

    Updated app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
-import './App.css'
-import Header from './components/header/Header'
-import Home from './components/home/Home'
-import About from './components/about/About'
-import Skills from './components/skills/Skills'
-import Services from './components/services/Services'
-import Qualification from './components/qualification/Qualification'
-import Contact from './components/contact/Contact'
+import React from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Services from "./components/services/Services";
+import Qualification from "./components/qualification/Qualification";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import ScrollUp from "./components/scrollup/ScrollUp";
+import Project from "./components/projects/Project";
 
 const App = () => {
   return (
     <>
-        <Header />
-        <main className='main'>
+      <Header />
+      <main className="main">
         <Home />
         <About />
         <Skills />
         <Services />
         <Qualification />
+        <Project />
         <Contact />
-        </main>
+      </main>
+      <Footer />
+      <ScrollUp />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
